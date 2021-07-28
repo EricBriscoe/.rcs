1076cd39dfdf2e0802f88e7a8f57f9bdb77cec76
Eric Briscoe <eric0briscoe@gmail.com>
2021-07-28 16:55:58 -0500

    fix broken thing

--- init.lua
+++ init.lua
@@ -36,7 +36,7 @@ local paq = require('paq-nvim').paq
 paq{'savq/paq-nvim', opt=true}
 paq 'shougo/deoplete-lsp'
 paq {'shougo/deoplete.nvim', run=fn['remote#host#UpdateRemotePlugins']}
-paq 'nvim-treesitter/nvim-treesitter'
+-- paq 'nvim-treesitter/nvim-treesitter'
 paq 'neovim/nvim-lspconfig'
 paq {'junegunn/fzf', run=fn['fzf#install']}
 paq 'junegunn/fzf.vim'
@@ -69,12 +69,12 @@ g['deoplete#enable_at_startup'] = 1
 -- DEOPLETE --
 
 -- LSP --
-local lsp = require 'lspconfig'
+local lsp = require'lspconfig'
 lsp.ccls.setup {}
 --lsp.jedi_language_server.setup{
 	--root_dir = lsp.util.root_pattern('.git', fn.getcwd())
 --}
-lsp.pyls.setup {
+lsp.pylsp.setup {
 	root_dir = lsp.util.root_pattern('.git', fn.getcwd()),
 	settings = {
 		pyls = {
